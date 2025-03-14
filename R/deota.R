c11dc85eb74aaa1d8b972174e4cbecbfca42cd76
Josh Berlinski <josh.berlinski@gmail.com>
2025-03-13 21:34:36 -0400

    add deota data to package

---
+++ R/deota.R
@@ -0,0 +1,20 @@
+#' Phases of clock genes
+#'
+#' Data from an experiment performed on mice to determine compare
+#' the peak phase times of circadian clock genes under ad-libitum
+#' and time-restricted feeding regimines. The two imported data frames
+#' are `alf` and `trf` corresponding to ad-libitum and time-restricted
+#' results, respectively. The observations are measured in degrees,
+#' corresponding to time on a 24-hour zeigeber time clock cycle.
+#'
+#' @docType data
+#'
+#' @usage data(deota)
+#'
+#' @references Deota S, Lin T, Chaix A, Williams A, Le H, Calligara H,
+#' Ramasamy R, Huang L, Panda S (2023) Diurnal transcriptome landscape
+#' of a multi-tissue response to time-restricted feeding in mammals.
+#' Cell Metabolism 35(1):150-164.e4
+#'
+#' @source \href{https://doi.org/10.1016/j.cmet.2022.12.006}
+"deota"
